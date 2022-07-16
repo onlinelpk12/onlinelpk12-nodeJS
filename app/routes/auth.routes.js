70efb13e08a949d4ae63d7ce7c86c0c5130ae2b2
vmatti <saichandu.matti@outlook.com>
2022-07-16 18:11:24 -0500

    cors lib test

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,20 +1,24 @@
+import cors from "cors"
+
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
+  app.use(cors());
+
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")  //changes for CORS Issue
+   // res.header("Access-Control-Allow-Origin", "*")  //changes for CORS Issue
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
-    if (req.method === "OPTIONS") {
+    /*if (req.method === "OPTIONS") {
       res.header(
         "Access-Control-Allow-Methods",
         "POST, PUT, PATCH, GET, DELETE"
       )
       return res.status(200).json({})
-    }
+    }*/
     next();
   });
 
